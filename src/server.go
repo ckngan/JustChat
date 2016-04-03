a01ae9ea73c4818d7dabddc31eaa3eced11c3bd0
brandensiegle <brandensiegle@hotmail.com>
2016-04-03 14:04:05 -0700

    added method functionality for JoinChatService

--- src/server.go
+++ src/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"net"
@@ -12,14 +11,14 @@ import (
 /*
 	----DECLARED TYPES----
 */
-//RPC Value for recieving messages
+//RPC Values
 type MessageService int
+type NodeService int
 
 //Client object
 type ClientItem struct {
-	username   string
-	recMsgConn *net.Conn
-	status     string
+	username    string
+	password 	string
 	nextClient *ClientItem
 }
 
@@ -42,11 +41,24 @@ type ServerReply struct {
 	Message string
 }
 
+//Retrun to client
+type ClientReply struct {
+	Message string
+}
+
+// address of chat server
+type ChatServer struct {
+	ServerName string
+	ServerRpcAddress string
+}
+
+
 /*
 	----GLOBAL VARIABLES----
 */
 //Net Info of this server
-var serverIPPort string
+var clientConnAddress string
+var nodeConnAdress string
 
 //List of clients
 var clientList *ClientItem
@@ -54,12 +66,14 @@ var clientList *ClientItem
 func main() {
 
 	// Parse arguments
-	usage := fmt.Sprintf("Usage: %s [server ip:port]\n", os.Args[0])
-	if len(os.Args) != 2 {
+	usage := fmt.Sprintf("Usage: %s [client ip:port] [server ip:port] \n", os.Args[0])
+	if len(os.Args) != 3 {
 		fmt.Printf(usage)
 		os.Exit(1)
 	}
-	serverIPPort = os.Args[1]
+
+	clientConnAddress = os.Args[1]
+	nodeConnAdress = os.Args[2]
 
 	//Initialize Clientlist
 	clientList = nil
@@ -68,7 +82,7 @@ func main() {
 	clientService := new(MessageService)
 	rpc.Register(clientService)
 
-	rpcListener, err := net.Listen("tcp", ":0")
+	rpcListener, err := net.Listen("tcp", clientConnAddress)
 	if err != nil {
 		log.Fatal("listen error:", err)
 	}
@@ -85,68 +99,30 @@ func main() {
 		}
 	}()
 
-	//Handle client connection setup
-	clientSetupListener, err := net.Listen("tcp", serverIPPort)
+	//setup to accept rpcCalls from message servers
+	messageNodeService := new(NodeService)
+	rpc.Register(messageNodeService)
+
+	//Handle message/storage connection setup
+	messageNodeListener, err := net.Listen("tcp", nodeConnAdress)
 	checkError(err)
 
 	for {
-		newClientConn, err := clientSetupListener.Accept()
+		messageNodeConn, err := messageNodeListener.Accept()
 		checkError(err)
-		println("New Client Connection")
-		//setupNewClient(net.conn, rpcAddress)
-		fmt.Println(rpcListener.Addr().String())
-		go setupNewClient(&newClientConn, rpcListener.Addr().String())
+		if err != nil {
+			log.Fatal("Connection error:", err)
+		}
+		go rpc.ServeConn(messageNodeConn)
 	}
-
-	/*otherListen, err := net.Listen("tcp", "localhost:10000")
-	checkError(err)
-	for {
-		//println("Blocking for Listen")
-		_, err := otherListen.Accept();
-		checkError(err)
-	}*/
-
-	//println("END")
-
 }
 
 /*
 	LOCAL HELPER FUNCTIONS
 */
-func setupNewClient(clientConn *net.Conn, rpcAddrString string) {
-	//Send the information for the client to make RPC Calls to server
-	//     format of data is [IP]:[PORT]
-	//     depending on implementation, may have to split to obtain port
-	userName, _, err := bufio.NewReader(*clientConn).ReadLine()
-	checkError(err)
-	println("Got username: " + string(userName))
+func addClientToList(username string, password string) {
 
-	connectionAttempts := 1
-
-	for usernameTaken(string(userName)) {
-		if connectionAttempts == 5 {
-			(*clientConn).Close()
-			return
-		}
-
-		(*clientConn).Write([]byte("USERNAME-TAKEN" + "\n"))
-		println("Letting client know username is taken")
-		userName, _, err = bufio.NewReader(*clientConn).ReadLine()
-		checkError(err)
-		println("Got new username: " + string(userName))
-		connectionAttempts++
-	}
-
-	(*clientConn).Write([]byte("Welcome:" + string(userName) + "\n"))
-	(*clientConn).Write([]byte("rpcAddress-"+ rpcAddrString + "\n"))
-	println("Welcoming " + string(userName))
-
-	addClientToList(string(userName))
-}
-
-func addClientToList(username string) {
-
-	newClient := &ClientItem{username, nil, "ONLINE", nil}
+	newClient := &ClientItem{username, password, nil}
 
 	if clientList == nil {
 		clientList = newClient
@@ -168,14 +144,24 @@ func addClientToList(username string) {
 	return
 }
 
-func usernameTaken(username string) bool {
+func authenticateFailure(username string, password string) bool {
 	next := clientList
+
+	//check to see if username exists
 	for next != nil {
 		if (*next).username == username {
+			if (*next).password == password {
+				//username match and password match
+				return false
+			}
+			//username exists but password doesn't match
 			return true
 		}
 		next = (*next).nextClient
 	}
+
+
+
 	return false
 }
 
@@ -193,21 +179,55 @@ func (msgSvc *MessageService) JoinChatService(message *NewClientSetup, reply *Se
 
 	// if user name not taken, server dials RPC address in message.RPCAddress
 	// and updates client with new rpc address, then replies WELCOME
+	// unless there is error dialing RPC to client then replies DIAL-ERROR
 	// otherwise, server replies, USERNAME-TAKEN
 
+	//check username
+	//if taken reply username taken
+	if ( authenticateFailure(message.UserName, message.Password) ){
+
+		reply.Message = "USERNAME-TAKEN";
+
+	//else dial rpc
+	} else {
+
+		clientConn, err := rpc.Dial("tcp", message.RpcAddress)
+		if(err != nil){
+			reply.Message = "DIAL-ERROR"
+			return nil
+		}
+
+
+		var clientReply ClientReply
+		var rpcUpdateMessage ChatServer 
+
+		//Dial and update the cient with their server address
+		rpcUpdateMessage.ServerName = "NameOfServer"
+		rpcUpdateMessage.ServerRpcAddress = ":7000"
+
+		callErr := clientConn.Call("ClientMessageService.UpdateRpcChatServer", rpcUpdateMessage, &clientReply)
+		if(callErr != nil){
+			reply.Message = "DIAL-ERROR"
+			return nil
+		}
+
+
+		reply.Message = "WELCOME";
+
+	}
+
+	
+
+
+
 	return nil
 }
 
-//Function for recieving a message from a client
-func (msgSvc *MessageService) SendMessage(message *ClientMessage, reply *ServerReply) error {
 
-	sendToAllClients(message.UserName, message.Message)
+/* 
+		CHECK for ERRORS
 
-	reply.Message = "Message Recieved"
-
-	return nil
-}
-
+*/
 func checkError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error ", err.Error())
